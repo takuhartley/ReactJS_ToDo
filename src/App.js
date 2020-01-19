16c849d918a2a43d1b9f371a28e2c64ca0c6e807
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-01-19 19:07:45 +0900

    Done with project

--- src/App.js
+++ src/App.js
@@ -1,33 +1,79 @@
-import React, { Component }from 'react';
-import './App.css';
-import Todos from './Components/Todos';
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from "react";
+import axios from "axios";
+import AddTodo from "./Components/AddTodo";
+import About from "./Components/Pages/About";
+import Header from "./Components/Layout/Header";
+import Todos from "./Components/Todos";
+import "./App.css";
+// import uuid from 'uuid';
 class App extends Component {
   state = {
-    todos: [
-      {
-        id: 1,
-        title: 'Get Coffee',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Read Medium Articles',
-        completed: true
-      },
-      {
-        id: 3,
-        title: 'Make a React ToDo site',
-        completed: false
-      }
-    ]
+    todos: []
+  };
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then(res => this.setState({ todos: res.data }));
   }
-  render () {
+  // Toggle Complete
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+  // Delete Todo
+  delTodo = id => {
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`).then(res =>
+      this.setState({
+        todos: [...this.state.todos.filter(todo => todo.id !== id)]
+      })
+    );
+  };
+  // Add Todo
+  addTodo = title => {
+    // const newTodo = {
+    //   id: uuid.v4(),
+    //   title,
+    //   completed: false
+    // }
+    axios
+      .post("https://jsonplaceholder.typicode.com/todos", {
+        title,
+        completed: false
+      })
+      .then(res => this.setState({ todos: [...this.state.todos, res.data] }));
+  };
+  render() {
     return (
-      <div className="App">
-        <Todos todos ={this.state.todos} />
-      </div>
-    )
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </Router>
+    );
   }
 }
 
--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,26 +1,35 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class TodoItem extends Component {
-    getStyle = () => {
-        return {
-            backgroundColor: '#f4f4f4',
-            padding: '10px',
-            borderBottom: '1px #ccc dotted',
-            textDecoration: this.props.todo.completed ? 'line-through' : 'none'
-        }
-    }
+  getStyle = () => {
+    return {
+      backgroundColor: "#f4f4f4",
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: this.props.todo.completed ? "line-through" : "none"
+    };
+  };
 
-    render() {
-        return (
-            <div style = {this.getStyle()}>
-                <p>
-                    <input type="checkbox" onChange={this.props.markComplete}/> {'   '}
-                    { this.props.todo.title }
-                </p>
-            </div>
-        )
-    }
+  render() {
+    // Destructuring
+    const { id, title } = this.props.todo;
+    return (
+      <div style={this.getStyle()}>
+        <p>
+          <input
+            type="checkbox"
+            onChange={this.props.markComplete.bind(this, id)}
+          />{" "}
+          {"   "}
+          {title}
+          <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>
+            X
+          </button>
+        </p>
+      </div>
+    );
+  }
 }
 
 // const itemStyle = {
@@ -28,7 +37,18 @@ export class TodoItem extends Component {
 // }
 // PropTypes
 TodoItem.propTypes = {
-    todo: PropTypes.object.isRequired
-}
+  todo: PropTypes.object.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired
+};
 
-export default TodoItem
+const btnStyle = {
+  background: "#ff0000",
+  color: "#fff",
+  border: "none",
+  padding: "5px 9px",
+  borderRadius: "50%",
+  cursor: "pointer",
+  float: "right"
+};
+export default TodoItem;
